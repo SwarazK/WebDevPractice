8a26a4c243a34de1da6818375d93ab345a2bc2b1
Swaraj Kausik <swarazk9@gmail.com>
2021-07-23 11:54:09 +0530

    BlackJack: Styling of Elements completed, some basic JS written

---
+++ jspractice/script2.js
@@ -0,0 +1,50 @@
+// function getCard() {
+//     let card = Math.floor(Math.random() * 13 ) + 1;
+//     if(card == 1) return 11;
+//     else if(card>=11 && card<=13) return 10;
+//     else return card;
+// }
+
+// function gen(){
+//     let firstCard = getCard();
+//     let secondCard = getCard();
+//     let sum = firstCard + secondCard;
+//     console.log(sum);
+//     if(sum > 21) console.log("You're out!!");
+//     else if(sum == 21) console.log("BlackJack!!!");
+//     else console.log("Do you want to draw a new card?");
+// } 
+// =================================================================================================
+
+// let myObject = {
+//     size : 6,
+//     name : "big",
+//     why : "yes"
+// }
+
+// console.log(myObject.size + " " + myObject.name + " " + myObject.why);
+// 
+// =================================================================================================
+
+let cards = [];
+let player = {
+    name : "Player",
+    chips : 500
+};
+
+
+function checkState{
+    
+}
+
+function drawCard{
+
+}
+
+function startGame{
+
+}
+
+function newGame{
+
+}
